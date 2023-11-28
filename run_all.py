c8c8ce0d8305a0e56d55da559ac56a3efbef8947
gerardgrau <gerardgraugarcia@gmail.com>
2023-11-28 22:40:25 +0100

    Modified python files

--- run_all.py
+++ run_all.py
@@ -1,23 +1,15 @@
-import subprocess
-from run_file import get_points
-
-
-def compile_file(alg):
-    try:
-        subprocess.run(["g++", alg+'.cc', "-o", alg, "-O3", "-Wall"], check=True)
-    
-    except subprocess.CalledProcessError as e:
-        print(f"Error compiling the file: {e}")
-    
+from run_file import get_points, compile_file
+import sys
 
 def main():
-    alg = 'greedy'
+    alg = sys.argv[1]
     compile_file(alg)
 
-    for diff in ('easy', 'med', 'hard'):
+    difficulties = ('easy', 'med', 'hard') if alg != 'exh' else ('easy', 'med')
+    
+    for diff in difficulties:
         avg = int( sum( get_points(alg, diff, str(i)) for i in range(1, 8) ) / 7 )
-        # formatted_avg = f'{avg:,}'
-        print(f'{diff: >4} : {avg: >11,}')
+        print(f'{diff: >4} : {avg: >4}')
 
 if __name__ == '__main__':
     main()
--- run_file.py
+++ run_file.py
@@ -18,14 +18,27 @@ def get_points(alg, diff, num):
 
     with open('output.txt', 'r') as file:
         content = file.read()
-        points_line = content.split('\n')[-2]
+        points_line = content.split('\n')[-3]
         points = int(points_line.split()[-1])
 
     return points
 
 
+def compile_file(alg):
+    try:
+        subprocess.run(["g++", alg+'.cc', "-o", alg, "-O3", "-Wall"], check=True)
+    
+    except subprocess.CalledProcessError as e:
+        print(f"Error compiling the file: {e}")
+    
+
+
 def main():
     alg, diff, num = sys.argv[1:4]
+
+    if len(sys.argv) < 4 or sys.argv[4] != 'nc': # nc = no compilation
+        compile_file(alg)
+        
     points = get_points(alg, diff, num)
     print(f'Points: {points}')
 

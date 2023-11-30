12eba3459bb0af1b98780f9923905b77c1907ff6
gerardgrau <gerardgraugarcia@gmail.com>
2023-11-30 17:24:22 +0100

    Changed Player from a Structure to a Class, added alpha as a static parameter of Player. Changed the visibility of some functions in Solution.

--- exh.cc
+++ exh.cc
@@ -12,30 +12,33 @@ using namespace std;
 
 
 // Global variables
-const vector<string> positions = {"por", "def", "mig", "dav"}; // all the possible positions
-const map<string, string> pos_to_CAPS = {{"por","POR"}, {"def","DEF"}, {"mig","MIG"}, {"dav","DAV"}};
+const vector<string> positions = {"por", "def", "mig", "dav"}; // List of all the positions: ["por", "def", "mig", "dav"] 
+const map<string, string> pos_to_CAPS = {{"por","POR"}, {"def","DEF"}, {"mig","MIG"}, {"dav","DAV"}}; // Map to convert the position to capital letters
 
-string output_filename;
-chrono::time_point <chrono::high_resolution_clock> start;
+string output_file;
+chrono::time_point <chrono::high_resolution_clock> start_time;
 
 
-struct Player
+class Player
 {
+public:
+    static inline double alpha;
     string name;
     string pos;
     int price;
     int points;
 
-    bool operator==(const Player& other) const {
-        // Compare the members that define equality for Player objects
-        return name == other.name && pos == other.pos && price == other.price && points == other.points;
+    Player(const string& name, const string& pos, int price, int points)
+        : name(name), pos(pos), price(price), points(points) {}
+
+    bool operator== (const Player& other) const {
+        return name == other.name and pos == other.pos and price == other.price and points == other.points;
     }
 
+    bool operator< (const Player& other) const {
+        return pow(points, alpha + 1) / price < pow(other.points, alpha + 1) / other.price;
+    }
 };
-
-using PlayerList = vector<Player>; // vector of players
-using PlayerMap = map<string, PlayerList>; // map of players by position
-PlayerMap players_map; // Global variable to store all the players
 
 
 struct Query
@@ -51,8 +54,13 @@ struct Query
 Query query; // global variable to store the query given
 
 
-class Solution {
+using PlayerList = vector<Player>; // vector of players
+using PlayerMap = map<string, PlayerList>; // map of players by position
+PlayerMap players_map; // Global variable to store all the players
 
+
+class Solution
+{
 private:
     map<string, PlayerList> players;
     int cost;
@@ -66,61 +74,53 @@ public:
         }
     }
 
-    void add_player(const Player& player) {
-        players[player.pos].push_back(player);
-        
-        cost += player.price;
-        points += player.points;
-    }
+    int get_cost() const { return cost; }
     
-    void pop_last_player(string pos) {
-        Player player = players[pos].back();
-        players[pos].pop_back();
+    int get_points() const { return points; }
 
-        cost -= player.price;
-        points -= player.points;
-    }
-
-    bool can_be_added(const Player& player) {
-        if (players[player.pos].size() + 1 > query.max_num_players[player.pos]) return false;
-        if (cost + player.price > query.max_cost) return false;
-
-        for (Player p : players[player.pos]) {
-            if (p == player) return false;
-        }
-        return true;
-    }
-
-    int get_cost() const {
-        return cost;
-    }
-    
-    int get_points() const {
-        return points;
-    }
-
-    uint get_size() {
-        uint size = 0;
+    size_t get_size() const {
+        size_t size = 0;
         for (auto pos : positions) {
-            size += players[pos].size();
+            size += players.at(pos).size();
         }
         return size;
     }
 
-    // returns the next position to be added to the solution, using the order: por, def, mig, dav
-    string get_pos_to_add() {
+    // Adds a player to the solution if it is possible to do so.
+    // @returns true if the player was added, false otherwise. 
+    bool add_if_possible(const Player& player) {
+        if (can_be_added(player)) {
+            add_player(player);
+            return true;
+        }
+        return false;
+    }
+
+    // Removes the last player added to the solution
+    // @param pos: the position of the player to be removed
+    void pop_last_player_at(string pos) {
+        Player last_player = players[pos].back();
+        players[pos].pop_back();
+
+        cost -= last_player.price;
+        points -= last_player.points;
+    }
+
+    // Returns the next position to be added to the solution, using the order: por, def, mig, dav
+    string get_pos_to_add() const {
         for (auto pos : positions) {
-            if (players[pos].size() < query.max_num_players[pos]) return pos;
+            if (players.at(pos).size() < query.max_num_players[pos]) return pos;
         }
         
-        assert(false); // this should never happen since we check that the solution is not complete before calling this function
+        assert(false); // the solution is not complete before calling this function
     }
 
     // Writes the solution in the output file
-    void write() { 
-        ofstream output(output_filename);
-        auto end = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    void write() const { 
+        ofstream output(output_file);
+
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
         output << fixed;
         output.precision(1);
         output << duration/1000.0 << endl;
@@ -137,16 +137,31 @@ public:
 
 private:
 
-    // Writes the players of a given position in the output file
-    void write_players(string pos, ofstream& output) {
+    void add_player(const Player& player) {
+        players[player.pos].push_back(player);
+        
+        cost += player.price;
+        points += player.points;
+    }
+    
+    bool can_be_added(const Player& player) const {
+        if (players.at(player.pos).size() + 1 > query.max_num_players[player.pos]) return false;
+        if (cost + player.price > query.max_cost) return false;
+
+        for (Player p : players.at(player.pos)) {
+            if (p == player) return false;
+        }
+        return true;
+    }
+
+    // Writes the players of a given position in the output files
+    // @param pos: the position of the players to be written
+    void write_players(string pos, ofstream& output) const {
         bool first = true;
-        for (Player p : players[pos]) {
-            if (first) {
-                first = false;
-                output << p.name;
-            } else {
-                output << ";" << p.name;
-            }
+        for (Player p : players.at(pos)) {
+            if (first) first = false;
+            else output << ";";
+            output << p.name;
         }
         output << endl;
     }
@@ -156,8 +171,8 @@ private:
 Solution best_solution; // global variable to store the best solution found so far
 
 
-Query read_query(const string& input_query) {
-    ifstream file(input_query);
+Query read_query(const string& query_file) {
+    ifstream file(query_file);
     int N1, N2, N3, max_cost, max_price_per_player;
     file >> N1 >> N2 >> N3 >> max_cost >> max_price_per_player;
     return {N1, N2, N3, max_cost, max_price_per_player};
@@ -198,9 +213,7 @@ PlayerMap read_players_map()
 
     // sort each of the lists of players by a heuristic determining the best players to be considered first
     for (auto pos : positions) {
-        sort(players_map[pos].begin(), players_map[pos].end(), [](const Player& p1, const Player& p2) {
-            return pow(p1.points, 3) / p1.price > pow(p2.points, 3) / p2.price;
-        });
+        sort(players_map[pos].begin(), players_map[pos].end());
     }
 
   return players_map;
@@ -223,21 +236,19 @@ void exhaustive_search(Solution& solution, string prev_pos = "", uint last_index
         return;
     }
 
-    // this solution will have a higher cost than the maximum
     if (solution.get_cost() > query.max_cost) return;
 
     // search for the next position needed to complete the solution
     string pos = solution.get_pos_to_add();
     if (prev_pos != pos) last_index = 0;
     
-    // iterate over all possible players from the last player you have added to the solution (to avoid repeated partial solutions)
+    // iterate over all players from the last player added to the solution to avoid repeated partial solutions
     for (uint i = last_index; i < players_map[pos].size(); i++) {
         Player player = players_map[pos][i];
         
-        if (solution.can_be_added(player)) {
-            solution.add_player(player);
+        if (solution.add_if_possible(player)) {
             exhaustive_search(solution, pos, i+1);
-            solution.pop_last_player(player.pos);
+            solution.pop_last_player_at(player.pos);
         }
     }
 }
@@ -253,19 +264,17 @@ void exhaustive_search() {
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) { // If the number of the files is not correct
-        cout << "Us incorrecte. S'han de proporcionar 3 arguments: data_base, query_file, output_file" << endl;
-        return 1; 
-    }
-    
-    start = chrono::high_resolution_clock::now(); // start the timer
 
-    const string input_database = argv[1]; // players' database
-    const string input_query = argv[2]; // query input file
-    output_filename = argv[3]; // output file
+    start_time = chrono::high_resolution_clock::now(); // start the timer
 
-    query = read_query(input_query); // llegim la consulta    
-    players_map = read_players_map(); // store all the players' info
+    const string input_database = argv[1];
+    const string query_file = argv[2];
+    output_file = argv[3];
+
+    query = read_query(query_file);
+    Player::alpha = pow(query.max_cost / 1e7, 0.3); // sets the static variable alpha of the class Player
+
+    players_map = read_players_map();
     
     exhaustive_search(); // stores the best solution in the global variable solution
 }
--- greedy.cc
+++ greedy.cc
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <cassert>
 #include <chrono>
 #include <cmath>
 #include <fstream>
@@ -15,22 +14,31 @@ using namespace std;
 const vector<string> positions = {"por", "def", "mig", "dav"}; // all the possible positions
 const map<string, string> pos_to_CAPS = {{"por","POR"}, {"def","DEF"}, {"mig","MIG"}, {"dav","DAV"}};
 
-string output_filename;
-chrono::time_point <chrono::high_resolution_clock> start;
+string output_file;
+chrono::time_point <chrono::high_resolution_clock> start_time;
 
 
-struct Player
+class Player
 {
+public:
+    static inline double alpha;
     string name;
     string pos;
     int price;
     int points;
 
-    bool operator==(const Player& other) const {
-        // Compare the members that define equality for Player objects
-        return name == other.name && pos == other.pos && price == other.price && points == other.points;
+    Player () {}
+
+    Player(const string& name, const string& pos, int price, int points)
+        : name(name), pos(pos), price(price), points(points) {}
+
+    bool operator== (const Player& other) const {
+        return name == other.name and pos == other.pos and price == other.price and points == other.points;
     }
 
+    bool operator< (const Player& other) const {
+        return pow(points, alpha + 1) / price < pow(other.points, alpha + 1) / other.price;
+    }
 };
 
 using PlayerList = vector<Player>; // vector of players
@@ -65,68 +73,40 @@ public:
         }
     }
 
-    void add_player(const Player& player) {
-        players[player.pos].push_back(player);
-        
-        cost += player.price;
-        points += player.points;
-    }
-
-    bool can_be_added(const Player& player) {
-        if (players[player.pos].size() + 1 > query.max_num_players[player.pos]) return false;
-        if (cost + player.price > query.max_cost) return false;
-
-        for (Player p : players[player.pos]) {
-            if (p == player) return false;
-        }
-        return true;
-    }
-
-    uint get_size() {
-        uint size = 0;
-        for (auto pos : positions) {
-            size += players[pos].size();
-        }
-        return size;
-    }
-
     /*
      * Obtains the best player that can be added to the solution, and adds it to the solution.
      * It uses a heuristic that depends on the max_cost of the query
      * @returns false if no player can be added, true otherwise.
      */
     bool add_best_player() {
+
+    // filter out the players that can't (and will never be able to) be added
         player_list.erase(remove_if(player_list.begin(), player_list.end(), [this](Player p){
-            return not can_be_added(p); // filters out the players that can't be added
+            return not can_be_added(p);
         }), player_list.end());
 
         if (player_list.empty()) return false;
 
-        // alpha is a parameter that depends on the maximum cost of the team of medium test and query 
-        const double alpha = pow(query.max_cost / 1e7, 0.3); // sort by: points^alpha * density (density = points/price)
-        
         Player best_player;
         
-        if (get_size() < 10) { // we're not adding the last player
-            best_player = *max_element(player_list.begin(), player_list.end(), [alpha](const Player& p1, const Player& p2) {
-                return pow(p1.points, alpha + 1) / p1.price < pow(p2.points, alpha + 1) / p2.price; // sort by points^alpha * density,
-            });                                                                                     // where density = points/price
-        } else { // we're adding the last player
-            best_player = *max_element(player_list.begin(), player_list.end(), [alpha](const Player& p1, const Player& p2) {
+        if (get_size() == 10) { // last player to be added
+            best_player = *max_element(player_list.begin(), player_list.end(), [](const Player& p1, const Player& p2) {
                 return p1.points < p2.points; // get the player with most points
             });
         }
-
+        else {
+            best_player = *max_element(player_list.begin(), player_list.end()); // get the best player according to Player comparison operator
+        }
         add_player(best_player);
         return true;
     }
-
     
     // Writes the solution in the output file
     void write() { 
-        ofstream output(output_filename);
-        auto end = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        ofstream output(output_file);
+
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
         output << fixed;
         output.precision(1);
         output << duration/1000.0 << endl;
@@ -143,32 +123,41 @@ public:
 
 private:
 
-    // Writes the players of a given position in the output files
-    void write_players(string pos, ofstream& output) {
-        PlayerList all_players = players[pos];
-        // add fake players to complete the team
-        PlayerList fake_players = get_fake_players(pos);
-        all_players.insert(all_players.end(), fake_players.begin(), fake_players.end());
-        bool first = true;
-        for (Player p : all_players) {
-            if (first) {
-                first = false;
-                output << p.name;
-            } else {
-                output << ";" << p.name;
-            }
+    size_t get_size() const {
+        size_t size = 0;
+        for (auto pos : positions) {
+            size += players.at(pos).size();
         }
-        output << endl;
+        return size;
     }
 
-    PlayerList get_fake_players(string pos) {
-        PlayerList p_list;
-        uint num_players = query.max_num_players[pos] - players[pos].size();
-        for (uint i = 0; i < num_players; i++) {
-            Player player = {"Fake_"+pos+char('1'+i), pos, 0, 0};
-            p_list.push_back(player);
+    void add_player(const Player& player) {
+        players[player.pos].push_back(player);
+        
+        cost += player.price;
+        points += player.points;
+    }
+
+    bool can_be_added(const Player& player) const {
+        if (players.at(player.pos).size() + 1 > query.max_num_players[player.pos]) return false;
+        if (cost + player.price > query.max_cost) return false;
+
+        for (Player p : players.at(player.pos)) {
+            if (p == player) return false;
         }
-        return p_list;
+        return true;
+    }
+
+    // Writes the players of a given position in the output files
+    // @param pos: the position of the players to be written
+    void write_players(string pos, ofstream& output) {
+        bool first = true;
+        for (Player p : players[pos]) {
+            if (first) first = false;
+            else output << ";";
+            output << p.name;
+        }
+        output << endl;
     }
 
 };
@@ -176,8 +165,8 @@ private:
 Solution best_solution; // global variable to store the best solution found so far
 
 
-Query read_query(const string& input_query) {
-    ifstream file(input_query);
+Query read_query(const string& query_file) {
+    ifstream file(query_file);
     int N1, N2, N3, max_cost, max_price_per_player;
     file >> N1 >> N2 >> N3 >> max_cost >> max_price_per_player;
     return {N1, N2, N3, max_cost, max_price_per_player};
@@ -209,7 +198,7 @@ PlayerList read_players_list()
         getline(in,aux2);
         
         if (price > query.max_price_per_player) continue; // filter out the players with higher price than the maximum
-        if (points == 0) continue; // we don't store players that have 0 points (neither real player nor fake ones)
+        if (points == 0 and club != "FakeTeam") continue; // we don't store players that have 0 points (neither real player nor fake ones)
 
         Player player = {name, position, price, points};
         player_list.push_back(player);
@@ -225,26 +214,24 @@ PlayerList read_players_list()
  */
 void greedy() {
     Solution solution;
-    while(solution.add_best_player())
-        ;
+    while(solution.add_best_player()) {}
+
     solution.write();
 }
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) { // If the number of the files is not correct
-        cout << "Wrong usage, 3 arguments must be passed: data_base query_file output_file" << endl;
-        return 1; 
-    }
-    
-    start = chrono::high_resolution_clock::now(); // start the timer
 
-    const string input_database = argv[1]; // players' database
-    const string input_query = argv[2]; // query input file
-    output_filename = argv[3]; // output file
+    start_time = chrono::high_resolution_clock::now();
 
-    query = read_query(input_query); // llegim la consulta    
-    player_list = read_players_list(); // store all the players' info
+    const string input_database = argv[1];
+    const string query_file = argv[2];
+    output_file = argv[3];
 
-    greedy(); //greedy algorithm
+    query = read_query(query_file);
+    Player::alpha = pow(query.max_cost / 1e7, 0.3); // heuristic parameter for the greedy algorithm
+
+    player_list = read_players_list();
+
+    greedy();
 }

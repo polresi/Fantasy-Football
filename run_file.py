fe9e485e0f9ec595af6131c7d79e8b97fe8a4234
gerardgrau <gerardgraugarcia@gmail.com>
2023-12-14 15:52:49 +0100

    When the execution is halted with ctrl+C, prints the points of the best solution.

--- run_file.py
+++ run_file.py
@@ -34,14 +34,21 @@ def compile_file(alg):
 
 
 def main():
-    alg, diff, num = sys.argv[1:4]
+    try:
+        alg, diff, num = sys.argv[1:4]
 
-    if len(sys.argv) <= 4 or sys.argv[4] != 'nc': # nc = no compilation
-        compile_file(alg)
-        
-    points = get_points(alg, diff, num)
-    print(f'Points: {points}')
+        if len(sys.argv) <= 4 or sys.argv[4] != 'nc': # nc = no compilation
+            compile_file(alg)
+
+        points = get_points(alg, diff, num)
+        print(f'Points: {points}')
     
+    except KeyboardInterrupt:
+        with open('output.txt', 'r') as file:
+            content = file.read()
+            points_line = content.split('\n')[-3]
+            points = int(points_line.split()[-1])
+        print(f'\nPoints: {points}')
 
 if __name__ == '__main__':
     main()
